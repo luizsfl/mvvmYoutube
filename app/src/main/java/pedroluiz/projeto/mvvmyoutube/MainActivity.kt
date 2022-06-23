ea45f24502b0629dbc133e59894b2737dfc5f1d2
Pedro Luiz <luizsfl@hotmail.com>
2022-06-23 12:54:38 -0300

    Criado opção para clicar no bottom navigation

--- app/src/main/java/pedroluiz/projeto/mvvmyoutube/MainActivity.kt
+++ app/src/main/java/pedroluiz/projeto/mvvmyoutube/MainActivity.kt
@@ -5,10 +5,12 @@ import android.content.Intent
 import android.net.Uri
 import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import pedroluiz.projeto.mvvmyoutube.Repository.MainRepository
@@ -16,6 +18,8 @@ import pedroluiz.projeto.mvvmyoutube.data.local.LiveDataBase
 import pedroluiz.projeto.mvvmyoutube.databinding.ActivityMainBinding
 import pedroluiz.projeto.mvvmyoutube.model.Live
 import pedroluiz.projeto.mvvmyoutube.data.remoto.rest.RetrofitService
+import pedroluiz.projeto.mvvmyoutube.ui.fragments.FavoritoFragment
+import pedroluiz.projeto.mvvmyoutube.ui.fragments.HomeFragment
 import pedroluiz.projeto.mvvmyoutube.viewModel.MainViewModelFactory.MainViewModelFactory
 import pedroluiz.projeto.mvvmyoutube.viewModel.main.MainViewModel
 
@@ -44,8 +48,35 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
             MainViewModel::class.java
         )
 
-        binding.recyclerview.adapter = adapter
+      //TODO  binding.recyclerview.adapter = adapter
 
+        val homeFragment = HomeFragment()
+        val favoritoFragment = FavoritoFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        binding.bottomNavigation.setOnNavigationItemReselectedListener { item ->
+            when(item.itemId) {
+                R.id.pag_home -> {
+                    // Respond to navigation item 1 click
+                    makeCurrentFragment(homeFragment)
+                    true
+                }
+                R.id.pag_favorito -> {
+                    // Respond to navigation item 2 click
+                    makeCurrentFragment(favoritoFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+    }
+
+    private fun makeCurrentFragment(fragment:Fragment) = supportFragmentManager.beginTransaction().apply {
+        replace(R.id.fl_wrapper,fragment)
+        commit()
     }
 
     override fun onStart() {
@@ -96,5 +127,7 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
         })
 
     }
+
+
 }
 
